98a7a34102ee3a818678e4782535261331823319
Fernando Giardina <fernando.giardina@etermax.com>
2016-05-18 09:33:35 -0300

    Initial Commit

---
+++ Example/Pods/Target Support Files/FGCLS/FGCLS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FGCLS : NSObject
+@end
+@implementation PodsDummy_FGCLS
+@end
---
+++ Example/Pods/Target Support Files/FGCLS/FGCLS-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double FGCLSVersionNumber;
+FOUNDATION_EXPORT const unsigned char FGCLSVersionString[];
+
